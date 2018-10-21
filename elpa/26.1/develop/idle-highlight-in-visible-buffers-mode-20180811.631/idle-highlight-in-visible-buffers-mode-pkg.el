0ea4cfd8ee278a0d8714a24556a3d00bf66c1538
spring <zhangxiuchun128@163.com>
2018-10-22 01:54:41 +0800

    添加当前光标所处文本语法高亮

---
+++ elpa/26.1/develop/idle-highlight-in-visible-buffers-mode-20180811.631/idle-highlight-in-visible-buffers-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "idle-highlight-in-visible-buffers-mode" "20180811.631" "highlight the word the point is on" 'nil :commit "09bb527ff9b8e5ad3da15aa461d595f187b91172" :keywords '("convenience") :authors '(("Ignacy Moryc")) :maintainer '("Ignacy Moryc") :url "https://github.com/ignacy/idle-highlight-in-visible-buffers")
